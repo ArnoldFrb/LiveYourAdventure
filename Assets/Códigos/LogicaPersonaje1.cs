f18236ad2eb8f14d60fba300e5435f5c07c31aca
Wilson Reales <wreales@unicesar.edu.co>
2020-11-16 22:29:00 -0500

    Cambios en terreno Desierto

---
+++ Assets/Códigos/LogicaPersonaje1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPersonaje1 : MonoBehaviour
+{
+    public float VelocidadMovimiento = 5.0f;
+    public float VelocidadRotacion = 200.0f;
+    private Animator anim;
+    public float x, y;
+    public Rigidbody rb;
+    public float FuerzaDeSalto = 8f;
+    public bool puedoSaltar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        puedoSaltar = false;
+        anim = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * VelocidadRotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * VelocidadMovimiento);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+
+        if (puedoSaltar)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetBool("salte", true);
+                rb.AddForce(new Vector3(0, FuerzaDeSalto, 0), ForceMode.Impulse);
+            }
+
+            anim.SetBool("tocarSuelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+        }
+
+    }
+
+    public void EstoyCayendo()
+    {
+        anim.SetBool("tocarSuelo", false);
+        anim.SetBool("salte", false);
+    }
+}
---
+++ Assets/Códigos/LogicaPies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPies : MonoBehaviour
+{
+    public LogicaPersonaje1 logicapersonaje1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        logicapersonaje1.puedoSaltar = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        logicapersonaje1.puedoSaltar = false;
+    }
+}
